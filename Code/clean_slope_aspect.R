f6b9ad74e79c33dc86e272d1c1d39d682f08ef72
KMJorgensen <karolina.jorgensen@gmail.com>
2022-11-30 18:24:17 +0100

    initial commit

---
+++ Code/clean_slope_aspect.R
@@ -0,0 +1,29 @@
+# Clean slope and aspect data
+
+slope_aspect_raw <- read_csv2("Data/FUNDER_raw_slope_and_aspect_2022.csv")
+
+### Clean dataset ####
+# Rename columns
+# Remove non-existing plots (Fau2GB and Ulv1B)
+# Recode siteID:s
+
+slope_aspect <- slope_aspect_raw |>
+  rename(siteID = site) |>
+  select(siteID, blockID, plotID, treatment, slope, aspect, comment) |>
+  filter(plotID != "Fau2GB") |>
+  filter(plotID != "Ulv1B") |>
+  mutate(siteID = recode(siteID,
+                        # old name (replace) = valid name (do not change)
+                        'Gud' = "Gudmedalen",
+                        'Lav' = "Lavisdalen",
+                        'Ram' = "Rambera",
+                        'Ulv' = "Ulvehaugen",
+                        'Skj' = "Skjelingahaugen",
+                        'Alr' = "Alrust",
+                        'Arh' = "Arhelleren",
+                        'Fau' = "Fauske",
+                        'Hog' = "Hogsete",
+                        'Ovs' = "Ovstedalen",
+                        'Vik' = "Vikesland",
+                        'Ves' = "Veskre"))
+
---
+++ Code/clean_soil_depth_measurements.R
@@ -0,0 +1,21 @@
+# Clean soil depth measurements
+
+library(tidyverse)
+library(stringr)
+
+soil_depth_raw <- read_csv2("Data/FUNDER_raw_soil_depth_measurements_2022.csv")
+
+
+soil_depth <- soil_depth_raw |>
+  select(-block) |>
+  rename("1" = depth1, "2" = depth2, "3" = depth3) |>
+  relocate(treatment, .after = plotID) |>
+  pivot_longer(!c(siteID, treatment, plotID), names_to = "measurement", values_to = "depth_cm")
+
+
+# Replace "," woth "." as decimal deliminator
+soil_depth$depth_cm <- str_replace_all(soil_depth$depth_cm, ",", ".")
+
+# Force soil depth to be read as a numeric variable
+soil_depth$depth_cm <- as.numeric(soil_depth$depth_cm)
+
