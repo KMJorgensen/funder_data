39ff390fc549645c0ff55fe74a0fb3064f501c11
KMJorgensen <karolina.jorgensen@gmail.com>
2022-12-07 10:32:41 +0100

    added code to export clean dataset as .csv

--- Code/clean_soil_depth_measurements.R
+++ Code/clean_soil_depth_measurements.R
@@ -58,7 +58,7 @@ soil_depth_clean <- soil_depth |>
   filter(plotID != "Skj4C")
 
 
-
+write.csv(soil_depth_clean, file = "Clean_data/FUNDER_clean_soil_depth_measurements_2022.csv", row.names = FALSE)
 
 
 
