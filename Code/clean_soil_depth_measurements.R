c5eb263fc20f855909238230b81ef9db32d5206c
KMJorgensen <karolina.jorgensen@gmail.com>
2022-12-07 09:14:40 +0100

    fixed typo

--- Code/clean_soil_depth_measurements.R
+++ Code/clean_soil_depth_measurements.R
@@ -5,7 +5,6 @@ library(stringr)
 
 soil_depth_raw <- read_csv2("Data/FUNDER_raw_soil_depth_measurements_2022.csv")
 
-
 soil_depth <- soil_depth_raw |>
   select(-block) |>
   rename("1" = depth1, "2" = depth2, "3" = depth3) |>
@@ -13,7 +12,7 @@ soil_depth <- soil_depth_raw |>
   pivot_longer(!c(siteID, treatment, plotID), names_to = "measurement", values_to = "depth_cm")
 
 
-# Replace "," woth "." as decimal deliminator
+# Replace "," with "." as decimal deliminator
 soil_depth$depth_cm <- str_replace_all(soil_depth$depth_cm, ",", ".")
 
 # Force soil depth to be read as a numeric variable
