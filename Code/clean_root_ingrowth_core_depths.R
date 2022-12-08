821e60a25ea2a9d60f56a7b1fb441214817f5f57
KMJorgensen <karolina.jorgensen@gmail.com>
2022-12-08 10:09:53 +0100

    change siteIDs and added code to drop NA-measurements

--- Code/clean_root_ingrowth_core_depths.R
+++ Code/clean_root_ingrowth_core_depths.R
@@ -13,7 +13,27 @@ RIC_clean$siteID <- str_to_title(RIC_clean$siteID)
 RIC_clean <- RIC_clean |>
   mutate(blockID = paste(siteID, blockID, sep = "")) |>
   mutate(plotID = paste(blockID, treatment, sep = "")) |>
-  relocate(plotID, .after = blockID)
+  relocate(plotID, .after = blockID) |>
+  mutate(siteID = recode(siteID,
+                         # old name (replace) = valid name (do not change)
+                         'Gud' = "Gudmedalen",
+                         'Lav' = "Lavisdalen",
+                         'Ram' = "Rambera",
+                         'Ulv' = "Ulvehaugen",
+                         'Skj' = "Skjelingahaugen",
+                         'Alr' = "Alrust",
+                         'Arh' = "Arhelleren",
+                         'Fau' = "Fauske",
+                         'Hog' = "Hogsete",
+                         'Ovs' = "Ovstedalen",
+                         'Vik' = "Vikesland",
+                         'Ves' = "Veskre"))
 
 RIC_clean$RIC_cm <- gsub(",", ".", RIC_clean$RIC_cm)
 RIC_clean$RIC_cm <- as.numeric(RIC_clean$RIC_cm)
+
+# Drops rows without RIC core depth measurements
+#RIC_clean <- RIC_clean |>
+#  drop_na(RIC_cm)
+
+# Export cleaned dataset
